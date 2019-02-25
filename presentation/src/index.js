1e922b261c13b79d45c8d385ce46049550d48ea8
Marcin Krasowski <marcin.krasowski@gmail.com>
2019-02-25 20:23:01 +0100

    eslint added and first slide

--- presentation/src/index.js
+++ presentation/src/index.js
@@ -2,4 +2,6 @@ import Reveal from 'reveal.js'
 import 'reveal.js/css/reveal.css'
 import 'reveal.js/css/theme/black.css'
 
-Reveal.initialize();
+Reveal.initialize({
+  history: true
+})
--- presentation/webpack.config.js
+++ presentation/webpack.config.js
@@ -1,42 +1,42 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    devServer: {
-        contentBase: './dist'
-    },
-    plugins: [
-        new CleanWebpackPlugin(['dist']),
-        new HtmlWebpackPlugin({
-            template: 'index.html'
-          })
-    ],
-    module: {
-      rules: [
-        {
-          test: /.css$/,
-          use: [
-            'style-loader',
-            'css-loader'
-          ]
-        },
-        {
-          test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-          use: [{
-              loader: 'file-loader',
-              options: {
-                  name: '[name].[ext]',
-                  outputPath: 'fonts/'
-              }
-          }]
+  mode: 'development',
+  entry: './src/index.js',
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'fonts/'
+          }
+        }]
       }
-      ]
-    },
-    output: {
-      filename: 'main.js',
-      path: path.resolve(__dirname, 'dist')
-    }
- };
+    ]
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+}
