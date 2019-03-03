70302c93cf1d9db0d7e051c3c17d2af8a4c074f1
Marcin Krasowski <marcin.krasowski@gmail.com>
2019-03-03 02:14:59 +0100

    simple local policy

--- presentation/src/index.js
+++ presentation/src/index.js
@@ -5,8 +5,8 @@ import 'reveal.js/css/theme/black.css'
 Reveal.initialize({
   history: true,
   width: "100%",
-  height: "100%",
-  margin: 0,
+  height: "200%",
+  margin: 0.1,
   minScale: 1,
-  maxScale: 1
+  maxScale: 2
 })
