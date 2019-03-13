4a436f0dad8bc818318237e3034c80c06ab2ea58
Marcin Krasowski <marcin.krasowski@gmail.com>
2019-03-13 23:24:51 +0100

    added dangerous task

--- code/app02/dependency/src/main/java/dependency/DangerousTask.java
+++ code/app02/dependency/src/main/java/dependency/DangerousTask.java
@@ -1,4 +1,12 @@
 package dependency;
 
-public class DangerousLibrary {
+public class DangerousTask implements Runnable {
+
+
+    @Override
+    public void run() {
+        String secret_key_location = System.getProperty("SECRET_KEY_LOCATION");
+
+        System.out.println("Secret key location: " + secret_key_location);
+    }
 }
