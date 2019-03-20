0cc7c4ccb81dfd983e792248058ea5992f22d38a
Marcin Krasowski <marcin.krasowski@gmail.com>
2019-03-20 01:14:58 +0100

    change task type to callable

--- code/app02/dependency/src/main/java/dependency/DangerousTask.java
+++ code/app02/dependency/src/main/java/dependency/DangerousTask.java
@@ -4,18 +4,23 @@ import lombok.extern.slf4j.Slf4j;
 
 import javax.security.auth.login.LoginContext;
 import javax.security.auth.login.LoginException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.security.PrivilegedAction;
 import java.security.PrivilegedExceptionAction;
+import java.util.concurrent.Callable;
 
 @Slf4j
-public class DangerousTask implements PrivilegedExceptionAction {
+public class DangerousTask implements Callable<String> {
 
 
     @Override
-    public Object run() {
+    public String call() {
         try {
             String secret_key_location = System.getProperty("SECRET_KEY_LOCATION");
             System.out.println("Secret key location: " + secret_key_location);
+            byte[] bytes = Files.readAllBytes(Paths.get(secret_key_location));
+            return new String(bytes);
         }catch (Exception e) {
             LOGGER.error("Task execution error",e);
         }
