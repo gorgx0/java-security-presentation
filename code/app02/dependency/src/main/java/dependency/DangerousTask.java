0c0e622ba7e1e0bea8d2da900030da1cee3fe46c
Marcin Krasowski <marcin.krasowski@gmail.com>
2019-03-21 02:03:23 +0100

    local catch try needed

--- code/app02/dependency/src/main/java/dependency/DangerousTask.java
+++ code/app02/dependency/src/main/java/dependency/DangerousTask.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 
 import javax.security.auth.login.LoginContext;
 import javax.security.auth.login.LoginException;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.security.PrivilegedAction;
@@ -16,15 +17,17 @@ public class DangerousTask implements Callable<String> {
 
     @Override
     public String call() {
+        String ret = "";
         try {
             String secret_key_location = System.getProperty("SECRET_KEY_LOCATION");
             System.out.println("Secret key location: " + secret_key_location);
-            byte[] bytes = Files.readAllBytes(Paths.get(secret_key_location));
-            return new String(bytes);
-        }catch (Exception e) {
-            LOGGER.error("Task execution error",e);
+            byte[] bytes = new byte[0];
+            bytes = Files.readAllBytes(Paths.get(secret_key_location));
+            ret = new String(bytes);
+        } catch (Exception e) {
+            LOGGER.error("Error execting task",e);
         }
-        return null;
+        return ret;
     }
 
 }
