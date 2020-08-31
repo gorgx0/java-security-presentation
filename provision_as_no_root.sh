011cfda5866749a485725c658b05953afeabf8af
Marcin Krasowski <marcin.krasowski@gmail.com>
2020-08-31 14:44:28 +0200

    updated java versions

--- provision_as_no_root.sh
+++ provision_as_no_root.sh
@@ -3,6 +3,6 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
-sdk i java 8.0.201-oracle
-sdk i java 11.0.2-open
+sdk i java 8.0.265-open
+sdk i java 14.0.2.hs-adpt 
 sdk i gradle
